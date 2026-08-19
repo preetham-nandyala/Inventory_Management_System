7601d17812ec6bf0e044af65e75d02f8ef3fd364
preetham-nandyala <nandyala.preetham755@gmail.com>
2026-08-19 23:47:41 +0530

    Express server setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
